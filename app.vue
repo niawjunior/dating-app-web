eacb8e81908266fcfe61839435f8b90b40ab1769
pasupol.bunsaen <niawkung@gmail.com>
2023-02-20 14:45:16 +0700

    :tada:

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <Login />
+  </div>
+</template>
+
+<script lang="ts">
+import Login from "./components/Login.vue";
+
+export default defineComponent({
+  components: {
+    Login,
+  },
+});
+</script>
---
+++ components/Login.vue
@@ -0,0 +1,115 @@
+<template>
+  <head>
+    <title>{{ title }}</title>
+  </head>
+  <div
+    class="bg-gray-100 min-h-screen flex flex-col justify-center py-12 sm:px-6 lg:px-8 p-4"
+  >
+    <h2 class="text-center text-3xl font-extrabold text-gray-900">
+      {{ title }}
+    </h2>
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+      <p class="mt-2 text-center text-sm text-gray-600">
+        Or
+        <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
+          create a new account
+        </a>
+      </p>
+    </div>
+
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+        <form class="space-y-6" @submit.prevent="submitForm">
+          <div>
+            <label for="email" class="block text-sm font-medium text-gray-700">
+              Email address
+            </label>
+            <div class="mt-1">
+              <input
+                v-model="email"
+                id="email"
+                name="email"
+                type="email"
+                autocomplete="email"
+                required
+                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+          </div>
+
+          <div>
+            <label
+              for="password"
+              class="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <div class="mt-1">
+              <input
+                v-model="password"
+                id="password"
+                name="password"
+                type="password"
+                autocomplete="current-password"
+                required
+                class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+          </div>
+
+          <div class="flex items-center justify-between">
+            <div class="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+              />
+              <label for="remember-me" class="ml-2 block text-sm text-gray-900">
+                Remember me
+              </label>
+            </div>
+
+            <div class="text-sm">
+              <a
+                href="#"
+                class="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Forgot your password?
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Sign in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  setup() {
+    // Data for the email and password fields
+    const email = ref<string>("");
+    const password = ref<string>("");
+    const title = "Join our dating app and find your perfect match!";
+
+    // Function to submit the login form
+    const submitForm = () => {
+      // Add your form submission logic here
+      // For example, you could use an HTTP request to send the email and password to your API endpoint
+      // Once the user is authenticated, you can use Nuxt.js's built-in authentication features to set a token or session cookie
+    };
+
+    return { email, password, submitForm, title };
+  },
+});
+</script>
